2fede24943f525172df8c82b6e37faf91bf54b68
nicolasraffray <nraffray@gmail.com>
2020-07-04 14:25:21 +0100

    using conditionals in html to display title

--- blog/views.py
+++ blog/views.py
@@ -7,15 +7,14 @@ posts = [
     'author' : 'Nic',
     'title' : 'Blog Post 1',
     'content' : 'first post content',
-    'data': '4 July, 2020'
+    'date': '4 July, 2020'
   },
   {
     'author' : 'Jane',
     'title' : 'Blog Post 2',
     'content' : 'second post content',
-    'data': '5 July, 2020'
+    'date': '5 July, 2020'
   }
-
 ]
 
 # Create your views here.
@@ -28,4 +27,4 @@ def home(request):
   return render(request, 'blog/home.html', context)
 
 def about(request):
-  return render(request, 'blog/about.html')
+  return render(request, 'blog/about.html',{'title': "About Page"})
