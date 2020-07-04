1a9165231938b4d369d606ff71a4e0ad677663b2
nicolasraffray <nraffray@gmail.com>
2020-07-04 14:11:30 +0100

    using some context with dummy data

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render
 
+# Posts
+
+posts = [ 
+  {
+    'author' : 'Nic',
+    'title' : 'Blog Post 1',
+    'content' : 'first post content',
+    'data': '4 July, 2020'
+  },
+  {
+    'author' : 'Jane',
+    'title' : 'Blog Post 2',
+    'content' : 'second post content',
+    'data': '5 July, 2020'
+  }
+
+]
+
 # Create your views here.
 
 # Function for handling certain routes
 def home(request):
-  return render(request, 'blog/home.html')
+  context = {
+    'posts': posts
+  }
+  return render(request, 'blog/home.html', context)
 
 def about(request):
   return render(request, 'blog/about.html')
