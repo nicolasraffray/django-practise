832a70daa79b484c684c99b187efc4f83fe0f783
nicolasraffray <nraffray@gmail.com>
2020-08-05 21:43:41 +0100

    user validation added

--- users/views.py
+++ users/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
+# options:
+# message.debug
+# message.info
+# message.success
+# message.warning
+# message.error
 
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            messages.success(request, f'Account Created for {username}!')
+            return redirect('blog-home')
+    else:
+        form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
