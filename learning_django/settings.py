0eb13b7c3831344d74ad8837a013605daf6eaa90
nicolasraffray <nraffray@gmail.com>
2020-08-07 17:14:02 +0100

    profile template made

--- learning_django/settings.py
+++ learning_django/settings.py
@@ -121,6 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+# DIRECTORY WHERE UPLOADED FILES WILL BE SAVED
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+
 CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
